a122e4eef6147a0ef548b43a312084e31771b611
Adarsh-Kumar-Mishra <adarshmishra360@gmail.com>
2021-09-26 23:37:23 +0530

    Added welcome screen

---
+++ src/com/lockme/DisplayOption.java
@@ -0,0 +1,9 @@
+package com.lockme;
+
+public class DisplayOption {
+     public static void printWelcome() {
+    	 System.out.println("           Welcome to LockMe.com. \n"
+    	 		          + "    This application was developed by Adarsh kumar \n"
+    	 		          + "*******************************************************");
+     }
+}
---
+++ src/com/lockme/Main.java
@@ -0,0 +1,10 @@
+package com.lockme;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DisplayOption.printWelcome();
+
+	}
+
+}
