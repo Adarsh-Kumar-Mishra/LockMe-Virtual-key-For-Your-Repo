c9939e6f095b68c410e6436495c2aee0e8715efd
Adarsh-Kumar-Mishra <adarshmishra360@gmail.com>
2021-09-27 00:30:22 +0530

    Added File operations

--- src/com/lockme/DisplayOption.java
+++ src/com/lockme/DisplayOption.java
@@ -3,7 +3,13 @@ package com.lockme;
 public class DisplayOption {
      public static void printWelcome() {
     	 System.out.println("           Welcome to LockMe.com. \n"
-    	 		          + "    This application was developed by Adarsh kumar \n"
+    	 		          + "    This application is developed by Adarsh kumar \n"
     	 		          + "*******************************************************");
      }
+     public static void displayMenu() {
+    	 String menu = "\n\nSelect any option number from below and press Enter \n\n"
+    	 + "1) Retrieve all files inside \"main\" folder\n" + "2) Display menu for File operations\n"
+    	 + "3) Exit program\n";
+    	 System.out.println(menu);
+    	 }
 }
